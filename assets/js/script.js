4ea7f6cf8d66c0c997ee73b4385c3c86346ce640
hannahrajarao <hannahrajarao@gmail.com>
2023-05-19 21:49:10 -0400

    Add API functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 const API_key = "";
 const url = `http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_key}`;
 
-fetch(url)
-.then(response => response.json())
-.then(data => console.log(data));
+function callAPI(url) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => getAPIdata(data));
+}
+
+function getAPIdata(data) {
+
+}
