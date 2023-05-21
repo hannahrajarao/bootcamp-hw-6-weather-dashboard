1e1ab9f75bf638aa1dbb5efbb0d68458fce17d86
hannahrajarao <hannahrajarao@gmail.com>
2023-05-21 17:18:39 -0400

    Add functions for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,28 @@ function addToList(cityName) {
     $('#past-searches').append();
 }
 
-function showWeather() {
+function updateCityList() {
+    cities.sort();
+    $('#past-searches').empty();
+    for(var city of cities) {
+        var cityButton = $('<a>').attr('href', `?city=${city}`).attr('id',currentCity).attr('role','button');
+        cityButton.addClass('btn btn-secondary btn-block');
+        cityButton.text(city);
+        $('#past-searches').append(cityButton);
+    }
+}
+
+function setLocalStorageCities() {
+    localStorage.setItem('weather_app_cities', cities);
+}
+
+function updateLocalStorageCities() {
+    var cityStrings =  localStorage.getItem('weather_app_cities');
+    if(cityStrings !== null)
+        cities = cityStrings.split(',');
+}
+
+async function showWeather() {
     // call api to get coordinates from city
     const cityUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=5&appid=${API_key}`;
     await fetch(cityUrl).then(response => response.json()).then(data => getLatAndLon(data));
